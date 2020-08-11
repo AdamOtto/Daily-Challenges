32e82871e911242f1305879ace4ffe3b9d974efd
AdamOtto <adamotto@shaw.ca>
2020-08-10 18:31:06 -0600

    Add files via upload

--- Challenge21.py
+++ Challenge21.py
@@ -1,25 +1,33 @@
-"""
+'''
 Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
 
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
-"""
+'''
 
 
-# Solution 1.  Only works for base case.  Ran out of time.
-def findClasses(times):
-    #classes = []
-    #classes[0] = times[0]
-    classes = 1
-    for i in range(1,len(times)):
-        t1 = range(times[i][0], times[i][1] + 1)
-        for j in range(0,i + 1):
-            if i is not j:
-                if not(times[j][0] in t1 or times[j][1] in t1):
-                    classes -= 1
-                    break
-        classes += 1
-    return str(classes)
+def findRooms(times):
+    classroom = []
+    classroom.append([times[0]])
+
+    for i in range(1, len(times)):
+        newClassAdded = False
+        for j in classroom:
+            if not RoomOccupied(times[i], j):
+                j.append(times[i])
+                newClassAdded = True
+                break
+        if not newClassAdded:
+            classroom.append([times[i]])
+    print(len(classroom))
+    #print(classroom)
+
+def RoomOccupied(time, room):
+    tr = range(time[0], time[1] + 1)
+    for i in room:
+        if i[0] in tr or i[1] in tr:
+            return True
+    return False
 
 
-in1 = [(30, 75), (0, 50), (60, 150)]
-print(findClasses(in1))
+in1 = [(30, 75), (0, 50), (0, 29), (60, 150)]
+findRooms(in1)
