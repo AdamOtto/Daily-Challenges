be61f17b059536630c045bda718d661a92cd0c2b
AdamOtto <adamotto@shaw.ca>
2020-10-07 12:14:14 -0600

    Add files via upload

--- Challenge79.py
+++ Challenge79.py
@@ -4,22 +4,18 @@ become non-decreasing by modifying at most 1 element.
 '''
 
 def Solution(in1):
-    print(in1)
     less = 0
     i = 1
-    while i < len(in1):
+    while i < len(in1) and less < 2:
         if in1[i - 1] >= in1[i]:
             less += 1
-            in1[i] = in1[i - 1] + 1
-            i = 0
         i += 1
     
     if less < 2:
         print(True)
     else:
         print(False)
-    print(in1)
     
 
-in1 = [3,2,1]
+in1 = [10,5,7]
 Solution(in1)
