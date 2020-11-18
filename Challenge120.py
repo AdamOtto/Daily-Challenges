ec589136e79ccddc1270d5f6f0eb41a67a0a9c55
AdamOtto <adamotto@shaw.ca>
2020-11-18 15:37:05 -0700

    Add files via upload

---
+++ Challenge120.py
@@ -0,0 +1,47 @@
+'''
+Implement the singleton pattern with a twist.
+First, instead of storing one instance, store two instances.
+And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), return the second instance.
+'''
+
+class instance:
+    __instance1 = None
+    __instance2 = None
+    __Called = False
+    def __init__(self):
+        if instance.__instance1 != None:
+            raise Exception("Only one instance allowed.")
+        else:
+            instance.__instance1 = self
+            instance.__instance2 = instance2()
+            instance.__Called = False
+
+    
+    @staticmethod
+    def getInstance():
+        if instance.__Called == False:
+            instance.__Called = True
+            instance.__instance2.__Called = False
+            return instance.__instance2
+        elif instance.__instance2.__Called == False:
+            instance.__Called = False
+            instance.__instance2.__Called = True
+            return instance.__instance1
+
+class instance2:
+    __instance2 = None
+    __Called = False
+    def __init__(self):
+        if instance2.__instance2 != None:
+            raise Exception("Only one instance allowed.")
+        else:
+            instance2.__instance2 = self
+
+t = instance()
+print(t)
+t = instance.getInstance()
+print(t)
+t = instance.getInstance()
+print(t)
+t = instance.getInstance()
+print(t)
