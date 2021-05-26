e620c68d3a0a9899f934a7b7521e549c9bc31bc1
AdamOtto <adamotto@shaw.ca>
2021-05-26 07:26:22 -0600

    Add files via upload

---
+++ Challenge216.py
@@ -0,0 +1,80 @@
+"""
+Given a number in Roman numeral format, convert it to decimal.
+
+The values of Roman numerals are as follows:
+
+{
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1
+}
+
+In addition, note that the Roman numeral system uses subtractive notation for numbers such as IV and XL.
+
+For the input XIV, for instance, you should return 14.
+"""
+
+def RomanConvert(l):
+    if l == 'M':
+        return 1000
+    elif l == 'D':
+        return 500
+    elif l == 'C':
+        return 100
+    elif l == 'L':
+        return 50
+    elif l == 'X':
+        return 10
+    elif l == 'V':
+        return 5
+    elif l == 'I':
+        return 1
+
+def RomanTier(l):
+    if l == 'M':
+        return 10
+    elif l == 'D':
+        return 8
+    elif l == 'C':
+        return 7
+    elif l == 'L':
+        return 5
+    elif l == 'X':
+        return 4
+    elif l == 'V':
+        return 2
+    elif l == 'I':
+        return 1
+
+def Solution(ar):
+    l = len(ar)
+    i = l - 1
+    retVal = 0
+    while i >= 0:
+        if i == 0:
+            retVal += RomanConvert(ar[i])
+        elif RomanConvert(ar[i - 1]) < RomanConvert(ar[i]):
+            if RomanTier(ar[i - 1]) == RomanTier(ar[i]) - 1:
+                retVal += RomanConvert(ar[i]) - RomanConvert(ar[i - 1])
+                i -= 1
+            else:
+                return False
+        else:
+            retVal += RomanConvert(ar[i])
+        i -= 1
+    return retVal
+
+
+
+in1 = "XIV"
+print(Solution(in1))
+in1 = "XL"
+print(Solution(in1))
+in1 = "IL"
+print(Solution(in1))
+in1 = "MMMDCCXXIV"
+print(Solution(in1))
