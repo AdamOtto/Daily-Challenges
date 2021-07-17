e2f0de3a3d2f285a73bd89d33f3b954d3e444234
AdamOtto <adamotto@shaw.ca>
2021-07-17 07:19:03 -0600

    Add files via upload

---
+++ Challenge265.py
@@ -0,0 +1,46 @@
+"""
+MegaCorp wants to give bonuses to its employees based on how many lines of codes they have written.
+They would like to give the smallest positive amount to each worker consistent with the constraint
+that if a developer has written more lines of code than their neighbor,
+they should receive more money.
+
+Given an array representing a line of seats of employees at MegaCorp,
+determine how much each one should get paid.
+
+For example, given [10, 40, 200, 1000, 60, 30],
+you should return [1, 2, 3, 4, 2, 1].
+"""
+
+def Solution(ar):
+    l = len(ar)
+    retVal = [1] * l
+    localMax = 0
+    localMin = 0
+    for i in range(1, l):
+        if ar[i] < ar[i - 1]:
+            localMax = i - 1
+            count = 0
+            for j in range(localMin, localMax + 1):
+                retVal[j] += count
+                count += 1
+            if localMax == localMin:
+                retVal[localMax] += 1
+            localMin = i
+
+    if ar[l - 1] > ar[l - 2]:
+        retVal[l - 1] += 1
+
+    return retVal
+
+
+in1 = [10, 40, 200, 1000, 60, 30]
+print(Solution(in1))
+
+in1 = [10, 40, 10, 60, 10, 40]
+print(Solution(in1))
+
+in1 = [30, 10, 30, 10, 30, 10]
+print(Solution(in1))
+
+in1 = [1,2,3,2,1]
+print(Solution(in1))
