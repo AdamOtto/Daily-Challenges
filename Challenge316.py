b7d678251226adcd59a3a7a34d90706366b965f5
AdamOtto <adamotto@shaw.ca>
2021-09-09 16:23:43 -0600

    Add files via upload

---
+++ Challenge316.py
@@ -0,0 +1,33 @@
+"""
+You are given an array of length N, where each element i represents the
+number of ways we can produce i units of change. For example, [1, 0, 1, 1, 2]
+would indicate that there is only one way to make 0, 2, or 3 units, and two ways of making 4 units.
+
+Given such an array, determine the denominations that must be in use.
+In the case above, for example, there must be coins with value 2, 3, and 4.
+"""
+
+def Solution(ar):
+    l = len(ar)
+    retVal = []
+    
+    for i in range(1, l):
+        if ar[i] <= 0:
+            continue
+        retVal.append(i)
+        for j in reversed(range(i - 1, l - 1)):
+            ar[j] -= ar[j - i]
+    
+    return retVal
+
+# Return 2,3,4
+in1 = [1, 0, 1, 1, 2]
+print(Solution(in1))
+
+# Return 1, 4
+in1 = [1, 1, 1, 1, 2]
+print(Solution(in1))
+
+# Return 2, 5, 6, 10
+in1 = [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1]
+print(Solution(in1))
