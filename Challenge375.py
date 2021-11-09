9fb6693359449ab30f11970c5aca85db55f7618b
AdamOtto <adamotto@shaw.ca>
2021-11-09 09:50:01 -0700

    Add files via upload

---
+++ Challenge375.py
@@ -0,0 +1,36 @@
+"""
+The h-index is a metric used to measure the impact and
+productivity of a scientist or researcher.
+
+A scientist has index h if h of their N papers have at
+least h citations each, and the other N - h papers have no more than h citations each.
+If there are multiple possible values for h, the maximum value is used.
+
+Given an array of natural numbers, with each value representing the number
+of citations of a researcher's paper, return the h-index of that researcher.
+
+For example, if the array was:
+[4, 0, 0, 2, 3]
+This means the researcher has 5 papers with 4, 1, 0, 2, and 3 citations respectively.
+The h-index for this researcher is 2, since they have 2 papers with 
+at least 2 citations and the remaining 3 papers have no more than 2 citations.
+"""
+
+def Solution(ar):
+    ar = sorted(ar, reverse = True)
+    low = 0
+    high = len(ar) - 1
+    mid = int( (low + high) / 2)
+    while low < high:
+        if ar[mid] >= mid + 1:
+            low = mid + 1
+        else:
+            high = mid
+        mid = int( (low + high) / 2)
+    return low
+
+# Return 2
+print(Solution([4, 0, 0, 2, 3]))
+
+# Return 3
+print(Solution([3,0,6,1,5]))
