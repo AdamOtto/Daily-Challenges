4a0a91b84f406c8c1d5379ad6c01bc2780b3e79a
AdamOtto <adamotto@shaw.ca>
2021-11-20 06:47:41 -0700

    Add files via upload

---
+++ Challenge387.py
@@ -0,0 +1,6 @@
+"""
+How would you explain the difference between
+an API and SDK to a non-technical person?
+"""
+API = "An API is like a set of tools, like a hammer or a screwdriver"
+SDK = "An SDK is like a toolbox that holds all the tools/APIs that you need."
