72b10a0eebe19a6a15c7564d0c8faaef1513c583
AdamOtto <adamotto@shaw.ca>
2021-11-22 06:41:13 -0700

    Add files via upload

---
+++ Challenge388.py
@@ -0,0 +1,5 @@
+"""
+How would you explain web cookies to someone non-technical?
+"""
+
+Cookie = "A small bit of information that your browser holds to remember info about your visit.  Such as whether you're logged in or not."
