da47d4ca64c79cea8b27862a669f7c541391f0c5
AdamOtto <adamotto@shaw.ca>
2022-03-04 08:14:12 -0700

    Add files via upload

---
+++ Challenge481.py
@@ -0,0 +1,47 @@
+"""
+Given an arithmetic expression in Reverse Polish Notation,
+write a program to evaluate it.
+
+The expression is given as a list of numbers and operands.
+For example: [5, 3, '+'] should return 5 + 3 = 8.
+
+For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']
+should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
+"""
+
+def Solution(ar):
+    t = []
+    t.extend(ar)
+
+    while len(t) > 1:
+        i = findOperand(t)
+        op = t.pop(i)
+        val1 = t.pop(i - 1)
+        val2 = t.pop(i - 2)
+        t.insert(i - 2, Compute(op, val2, val1))
+    return t[0]
+
+def findOperand(ar):
+    for i in range(0, len(ar)):
+        if ar[i] == '+' or ar[i] == '-' or ar[i] == '/' or ar[i] == '*':
+            return i
+
+def Compute(operand, val1, val2):
+    if operand == "+":
+        return val1 + val2
+    elif operand == "-":
+        return val1 - val2
+    elif operand == "/":
+        return val1 / val2
+    elif operand == "*":
+        return val1 * val2
+
+
+# Return 8
+print(Solution([5, 3, '+']))
+
+# Return 5.0
+print(Solution([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']))
+
+# Return 5.0
+print(Solution([1, 1, '-', 2, '+', 4, '*', 2, '+', 2, '/']))
