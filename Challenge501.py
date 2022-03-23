c62b54c29151b34920964ee8b1334523e0a93cc4
AdamOtto <adamotto@shaw.ca>
2022-03-23 08:17:47 -0600

    Add files via upload

---
+++ Challenge501.py
@@ -0,0 +1,28 @@
+"""
+Given a function that generates perfectly random numbers between 1 and k (inclusive),
+where k is an input, write a function that shuffles a deck of cards represented
+as an array using only swaps.
+
+It should run in O(N) time.
+
+Hint: Make sure each one of the 52!
+permutations of the deck is equally likely.
+"""
+import random
+
+
+def Solution(ar):
+    l = len(ar)
+    for i in range(l):
+        swap = random.randint(1, l) - 1
+        temp = ar[i]
+        ar[i] = ar[swap]
+        ar[swap] = temp
+    return ar
+
+
+
+cards = []
+for i in range(0, 52):
+    cards.append(i)
+print(Solution(cards))
