ddc52f4df1373e7f1d44f98ceedf07e3b03cab29
AdamOtto <adamotto@shaw.ca>
2022-04-21 08:09:20 -0600

    Add files via upload

---
+++ Challenge529.py
@@ -0,0 +1,52 @@
+"""
+Given a string, split it into as few strings as
+possible such that each string is a palindrome.
+
+For example, given the input string racecarannakayak,
+return ["racecar", "anna", "kayak"].
+
+Given the input string abc, return ["a", "b", "c"].
+"""
+
+def Solution(in1):
+    l = len(in1)
+    retVal = []
+    d = {}
+    for i in reversed(range(0, l)):
+        if in1[i] not in d:
+            d[in1[i]] = []
+        d[in1[i]].append(i)
+
+    i = 0
+    while i < l:
+        noPalidromeFound = True
+        for index in d[in1[i]]:
+            if i != index and isPalindrome(in1, i, index):
+                retVal.append(in1[i:index+1])
+                noPalidromeFound = False
+                i = index
+                break
+        if noPalidromeFound:
+            retVal.append(in1[i])
+        i += 1
+    return retVal
+
+def isPalindrome(in1, i, j):
+    if i >= j:
+        return False
+    while i < j:
+        if in1[i] == in1[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
+
+# Return ['racecar', 'anna', 'kayak']
+print(Solution("racecarannakayak"))
+# Return ['a', 'b', 'c']
+print(Solution("abc"))
+# Return ['t', 'h', 'isi', 's', 'a', 't', 'e', 's', 't']
+print(Solution("thisisatest"))
+# Return ['tacocat'] 
+print(Solution("tacocat"))
