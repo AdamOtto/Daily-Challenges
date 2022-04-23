21e886886e1e00672b34889bfeb94c6be82aeea2
AdamOtto <adamotto@shaw.ca>
2022-04-23 06:07:32 -0600

    Add files via upload

---
+++ Challenge531.py
@@ -0,0 +1,21 @@
+"""
+Using a read7() method that returns 7 characters from a file,
+implement readN(n) which reads n characters.
+
+For example, given a file with the content “Hello world”,
+three read7() returns “Hello w”, “orld” and then “”.
+"""
+file = "Hello World"
+i = 0
+
+def read7():
+    global i
+    global file
+    print(file[i:max(len(file) - 1, i+7)])
+    i += 7
+
+
+read7()
+read7()
+read7()
+read7()
