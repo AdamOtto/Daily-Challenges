3f4a4d1c97ed0b33343d8716cafc14cdeb00dd1f
AdamOtto <adamotto@shaw.ca>
2022-04-27 07:54:11 -0600

    Add files via upload

---
+++ Challenge535.py
@@ -0,0 +1,45 @@
+"""
+You are given N identical eggs and access to a building with k floors.
+Your task is to find the lowest floor that will cause an egg to break,
+if dropped from that floor. Once an egg breaks, it cannot be dropped again.
+If an egg breaks when dropped from the xth floor,
+you can assume it will also break when dropped from any floor greater than x.
+
+Write an algorithm that finds the minimum number of trial drops it will take,
+in the worst case, to identify this floor.
+
+For example, if N = 1 and k = 5, we will need to try dropping the egg at every floor,
+beginning with the first, until we reach the fifth floor, so our solution will be 5.
+"""
+import sys
+
+def Solution(eggs, floors):
+    eggFloor = [[0 for i in range(floors + 1)] for j in range(eggs + 1)]
+
+    for i in range(1, eggs + 1):
+        eggFloor[i][1] = 1
+        eggFloor[i][0] = 0
+
+    for i in range(1, floors + 1):
+        eggFloor[1][i] = i
+
+    for i in range(2, eggs + 1):
+        for j in range(2, floors + 1):
+            eggFloor[i][j] = sys.maxsize
+            for k in range(1, j + 1):
+                temp = 1 + max(eggFloor[i - 1][k - 1], eggFloor[i][j - k])
+                if temp < eggFloor[i][j]:
+                    eggFloor[i][j] = temp
+    return eggFloor[eggs][floors]
+
+# Return 5
+print(Solution(1, 5))
+
+# Return 4
+print(Solution(2, 10))
+
+# Return 8
+print(Solution(4, 100))
+
+# Return 10
+print(Solution(50, 1000))
