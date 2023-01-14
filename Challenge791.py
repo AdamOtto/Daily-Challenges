5440bd8d19cf4abb8fd80f0cde50c6e9b8e8c19b
AdamOtto <adamotto@shaw.ca>
2023-01-14 06:45:59 -0700

    Add files via upload

---
+++ Challenge791.py
@@ -0,0 +1,25 @@
+"""
+Write a function, throw_dice(N, faces, total), that determines how many
+ways it is possible to throw N dice with some number of faces each to get a specific total.
+
+For example, throw_dice(3, 6, 7) should equal 15.
+"""
+def Solution(N, faces, total):
+    mem = [[0 for i in range(total+1)] for j in range(N+1)]
+    mem[0][0] = 1
+    
+    for i in range(1, N + 1):
+        for j in range(1, total + 1):
+            mem[i][j] = mem[i][j - 1] + mem[i - 1][j - 1]
+            if j - faces - 1 >= 0:
+                mem[i][j] -= mem[i - 1][j - faces - 1]
+    
+    return mem[N][total]
+# Return 15
+print(Solution(3, 6, 7))
+
+# Return 6
+print(Solution(2, 6, 7))
+
+# Return 12255
+print(Solution(5, 12, 34))
