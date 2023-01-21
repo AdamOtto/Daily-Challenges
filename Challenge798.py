cd01983950ae283b856ba95c322664d97905b93d
AdamOtto <adamotto@shaw.ca>
2023-01-21 07:36:54 -0700

    Add files via upload

---
+++ Challenge798.py
@@ -0,0 +1,46 @@
+"""
+You are given a set of synonyms, such as (big, large) and (eat, consume).
+Using this set, determine if two sentences with the same number of words are equivalent.
+For example, the following two sentences are equivalent:
+"He wants to eat food."
+"He wants to consume food."
+Note that the synonyms (a, b) and (a, c) do not necessarily imply (b, c): consider the case of (coach, bus) and (coach, teacher).
+Follow-up: what if we can assume that (a, b) and (a, c) do in fact imply (b, c)?
+"""
+def Solution(synonyms, sen1, sen2):
+    s1 = sen1.split(" ")
+    s2 = sen2.split(" ")
+    l = len(s1)
+    if l != len(s2):
+        return False
+    
+    for i in range(l):
+        if s1[i] != s2[i]:
+            if s1[i] not in synonyms or s2[i] not in synonyms:
+                return False
+    return True
+
+
+# Return True
+in1 = ["eat", "consume"]
+in2 = "He wants to eat food."
+in3 = "He wants to consume food."
+print(Solution(in1, in2, in3))
+
+# Return False
+in1 = ["eat", "consume"]
+in2 = "He wants to eat food."
+in3 = "Consume food, you must padawan."
+print(Solution(in1, in2, in3))
+
+# Return True
+in1 = ["big", "large"]
+in2 = "Whales are big"
+in3 = "Whales are large"
+print(Solution(in1, in2, in3))
+
+# Return False
+in1 = ["big", "huge"]
+in2 = "Whales are big"
+in3 = "Whales are large"
+print(Solution(in1, in2, in3))
