b1d67e7f6533e657a8a6981c9ab7d5f5499b7d64
AdamOtto <adamotto@shaw.ca>
2023-02-09 09:19:02 -0700

    Add files via upload

---
+++ Challenge817.py
@@ -0,0 +1,52 @@
+"""
+Given a dictionary of words and a string made up of those words (no spaces),
+return the original sentence in a list.
+If there is more than one possible reconstruction, return any of them.
+If there is no possible reconstruction, then return null.
+
+For example, given the set of words 'quick', 'brown', 'the', 'fox',
+and the string "thequickbrownfox",
+you should return ['the', 'quick', 'brown', 'fox'].
+
+Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond',
+and the string "bedbathandbeyond",
+return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+"""
+def Solution(d, words):
+    retVal = []
+    wend = 0
+    words = words.lower()
+    dic = {}
+    for i in range(len(d)):
+        dic[d[i].lower()] = d[i].lower()
+    
+    while wend < len(words) + 1:
+        for i in range(0, len(words) + 1):
+            if words[wend:i] in dic:
+                retVal.append(words[wend:i])
+                wend = i
+        wend += 1
+            
+    if len(retVal) == 0:
+        return None
+    return retVal
+
+# Return ['the', 'quick', 'brown', 'fox']
+in1 = ['the', 'quick', 'brown', 'fox']
+in2 = "thequickbrownfox"
+print(Solution(in1, in2))
+
+# Return ['bed', 'bath', 'and', 'beyond']
+in1 = ['bed', 'bath', 'bedbath', 'and', 'beyond']
+in2 = "bedbathandbeyond"
+print(Solution(in1, in2))
+
+# Return ['bed', 'and', 'bath', 'and', 'beyond']
+in1 = ['bed', 'bath', 'bedbath', 'and', 'beyond']
+in2 = "BEDandBATHandOHNOTHISCANTBEHEREbeyond"
+print(Solution(in1, in2))
+
+# Return None
+in1 = ['the', 'quick', 'brown', 'fox']
+in2 = "ohdonpiano"
+print(Solution(in1, in2))
