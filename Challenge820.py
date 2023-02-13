76870ed4a3c8321150d125bcd0b870d0e6d3b1b6
AdamOtto <adamotto@shaw.ca>
2023-02-13 08:09:42 -0700

    Add files via upload

--- Challenge820.py
+++ Challenge820.py
@@ -1,27 +1,38 @@
 """
-Given a array of numbers representing the stock prices of a company
-in chronological order, write a function that calculates the maximum
-profit you could have made from buying and selling that stock once.
-You must buy before you can sell it.
+Given an array of integers, find the first missing positive integer
+in linear time and constant space. In other words, find the lowest
+positive integer that does not exist in the array.
+The array can contain duplicates and negative numbers as well.
 
-For example, given [9, 11, 8, 5, 7, 10], you should return 5,
-since you could buy the stock at 5 dollars and sell it at 10 dollars.
+For example, the input [3, 4, -1, 1] should give 2.
+The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
 """
+# O(n)
 def Solution(ar):
-    l = len(ar)
-    if l < 2:
-        return None
-    retVal = 0
-    for i in range(l):
-        for j in range(i + 1, l):
-            retVal = max(retVal, ar[j] - ar[i])
-    return retVal
+    m = max(ar)
+    if m < 1:
+        return 1
+    if len(ar) == 1:
+        return m+1
+    
+    l = [0]*m
+    for x in ar:
+        if x > 0:
+            l[x-1] = x
+    if 0 in l:
+        return l.index(0) + 1
+    else:
+        return m + 1
 
-# Return 5
-print(Solution([9, 11, 8, 5, 7, 10]))
+# Return 2
+print(Solution([3, 4, -1, 1]))
 
-# Return 7
-print(Solution([8, 15,2,4]))
+# Return 3
+print(Solution([1, 2, 0]))
 
-# Return 0
-print(Solution([10,9,8,7,6,5,4,3,2,1,0]))
+# Return 743
+in1 = [i for i in range(1000)]
+in1.pop(743)
+print(Solution(in1))
