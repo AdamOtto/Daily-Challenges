e0b9f047fe4b76ebf111be2f7d3c76002def12b4
AdamOtto <adamotto@shaw.ca>
2023-04-12 08:05:18 -0600

    Add files via upload

---
+++ Challenge879.py
@@ -0,0 +1,26 @@
+"""
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+def Solution(ar, k):
+    ar = sorted(ar)
+    i = 0
+    j = len(ar) - 1
+    while i < j:
+        if ar[i] + ar[j] < k:
+            i += 1
+        elif ar[i] + ar[j] > k:
+            j -= 1
+        else:
+            return ar[i], ar[j]
+    return False
+    
+# Return (7, 10)
+print(Solution([10, 15, 3, 7], 17))
+# Return (6, 15)
+print(Solution([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 21))
+# Return False
+print(Solution([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 40))
