ce38f56053746d7b4ae6c3ad672ceccd2cdea55a
AdamOtto <adamotto@shaw.ca>
2023-07-30 06:39:52 -0600

    Add files via upload

---
+++ Challenge967.py
@@ -0,0 +1,28 @@
+"""
+You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}.
+By the pigeonhole principle, there must be a duplicate.
+Find it in linear time and space.
+"""
+# Linear time, constant space.
+def Solution(in1):
+    l = len(in1)
+    xorCount = 0
+    xorList = 0
+
+    for i in range(1, l):
+        xorCount = xorCount ^ i
+
+    for i in range(0, l):
+        xorList = xorList ^ in1[i]
+
+    return xorCount ^ xorList
+
+# Return 9
+print(Solution([1,4,3,2,5,9,8,7,9,6]))
+
+# Return 43
+in1 = []
+for i in range(1, 100):
+    in1.append(i)
+in1.insert(3, 43)
+print(Solution(in1))
