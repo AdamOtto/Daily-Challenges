f412dba54e7ec7e3d1bc37a0cfdcca2c4bbcf2ad
AdamOtto <adamotto@shaw.ca>
2023-09-13 09:03:01 -0600

    Add files via upload

---
+++ Challenge1002.py
@@ -0,0 +1,26 @@
+"""
+Given a sorted array, find the smallest positive integer
+that is not the sum of a subset of the array.
+
+For example, for the input [1, 2, 3, 10], you should return 7.
+
+Do this in O(N) time.
+"""
+
+def Solution(ar):
+    l = len(ar)
+    retVal = 1
+    for i in range(0, l):
+        if ar[i] <= retVal:
+            retVal = retVal + ar[i]
+        else:
+            break
+    return retVal
+
+# Return 7
+in1 = [1,2,3,10]
+print(Solution(in1))
+
+# Return 56
+in1 = [1,2,3,4,5,6,7,8,9,10]
+print(Solution(in1))
