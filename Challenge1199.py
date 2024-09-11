d17f176463a8e107a772b3dbdd530b188d3581de
AdamOtto <adamotto@shaw.ca>
2024-09-11 08:59:11 -0600

    Add files via upload

---
+++ Challenge1199.py
@@ -0,0 +1,16 @@
+"""
+Implement a job scheduler which takes in a function
+f and an integer n, and calls f after n milliseconds.
+"""
+import time
+
+def jobScheduler(f, n):
+    time.sleep(n * 0.001)
+    f()
+    
+    
+def sayHello ():
+    print("Hello")
+
+
+jobScheduler(sayHello, 1000)
