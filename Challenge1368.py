eee21795aefb2938d9287d9a92632c4b5354b300
AdamOtto <adamotto@shaw.ca>
2025-09-02 08:14:04 -0600

    Add files via upload

---
+++ Challenge1368.py
@@ -0,0 +1,9 @@
+"""
+Describe what happens when you type a URL into your browser and press Enter.
+"""
+
+"""
+After you type in the URL, the browser looks up the IP of the domain.
+If its found, it starts a TCP connection with the server, then requests the HTTP data.
+Once its received, the browser builds the website.
+"""
