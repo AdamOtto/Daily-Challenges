9da1d8367d3d5aa0b058f825a51eff150bf2abbb
AdamOtto <adamotto@shaw.ca>
2026-04-30 08:00:38 -0600

    Add files via upload

---
+++ Challenge1504.py
@@ -0,0 +1,36 @@
+"""
+Implement an efficient string matching algorithm.
+
+That is, given a string of length N and a pattern of length k,
+write a program that searches for the pattern in the string with
+less than O(N * k) worst-case time complexity.
+
+If the pattern is found, return the start index of its location.
+If not, return False.
+"""
+# O(n)
+def Solution(ar, pattern):
+    la = len(ar)
+    lp = len(pattern)
+    patHash = hash(pattern)
+    subStrHash = []
+    retVal = []
+    for i in range(0, la - lp + 1):
+        if patHash == hash(ar[i:i+lp]):
+            retVal.append(i)
+    return retVal
+
+# Return [0, 3, 7]
+in1 = "AABAABBAA"
+pat = "AA"
+print(Solution(in1, pat))
+
+# Return [3, 6, 13, 34, 41]
+in1 = "10010110110111011011101010100010101011011101101110101011001010001011"
+pat = "101101"
+print(Solution(in1, pat))
+
+# Return [2, 3]
+in1 = "CBAAAABC"
+pat = "AAA"
+print(Solution(in1, pat))
